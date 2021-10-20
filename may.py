ce4cb8879ae097776cdc2b63eb79879bc40b1e87
Kalinga <kalingakhatua@gmail.com>
2021-10-20 19:01:33 +0530

    Revamped speak function, sounds better with internet. Added youtube search and others

--- may.py
+++ may.py
@@ -1,10 +1,12 @@
 from win32com.client import Dispatch
 from pygame import mixer
 mixer.init()
+from playsound import playsound
 import speech_recognition as sprec
 import time
 import wikipedia
 import webbrowser
+import pafy
 import os
 import myweathercalls
 import requests
@@ -12,11 +14,34 @@ import json
 import re
 import random
 import threading
+from gtts import gTTS
 
 
-def speakmay(talkie):
+def speakmay1(talkie):
 	spk = Dispatch("SAPI.SpVoice")
 	spk.Speak(f'. . .{talkie}')
+
+def speakmay2(talkie):
+	mytalks = str(talkie)
+	language = 'hi'
+	myobj = gTTS(text=mytalks,lang=language,tld='co.in',slow=False)
+	date_string = time.strftime("%d%m%Y%H%M%S",time.localtime())
+	filename = "voice"+date_string+".mp3"
+	myobj.save(filename)
+	playsound(filename)
+	os.remove(filename)
+	# mixer.music.load("talkie.mp3")
+	# mixer.music.play()
+	# while mixer.music.get_busy():
+	# 	pass
+	# mixer.music.load("talkie_piggy.mp3")
+	# os.remove("talkie.mp3")
+
+def speakmay(talkie):
+	try:
+		speakmay2(talkie)
+	except Exception as e:
+		speakmay1(talkie)
 
 def wishmemay():
 	nowhr = int(time.strftime("%H",time.localtime()))
@@ -34,19 +59,28 @@ def wishmemay():
 def listen():
 	r = sprec.Recognizer()
 	with sprec.Microphone() as source:
-		print("Say.<3")
+		print("\nSay.<3")
 		r.pause_threshold = 1
-		# audio = r.listen(source,timeout=1,phrase_time_limit=None)
-		audio = r.listen(source,timeout=3,phrase_time_limit=30)
+		audio = r.listen(source,timeout=3,phrase_time_limit=10)
+		# audio = r.listen(source)
 
 	try:
-		print("Listening.")
+		print("Recognising...")
 		usertalks = r.recognize_google(audio, language="en-in")
-		print(f'You said : {usertalks}\n')
+		print(f'\n ~ {usertalks}\n')
 		return usertalks
 	except Exception as e:
-		print(e,"Say that again please.")
+		if breakingnum >= 5 and mixer.music.get_busy()==0 :
+			print("\nDon't talk to me. Bye.")
+			speakmay("Don't talk to me. Bye.")
+		else:
+			print("Say that again please.")
 		return None #not working as of now
+
+def ytfirsturlreturn(query):
+	results = requests.get(f'https://www.youtube.com/results?search_query={query.replace(" ","+")}').text
+	found = re.findall(r'{"videoId":"[-.\d\w]+', results)[0].split("\"")[3]
+	return f'https://youtu.be/{found}'
 
 def musicplay(loop):
 	musicdir = [ item for item in os.listdir(r'F:\Anik\My Musics\English musics') if os.path.splitext(item)[1] == ".mp3"]
@@ -73,9 +107,9 @@ def musicrandomplay(loop):
 	os.chdir(r'F:\Anik\My Musics\English musics')
 	# mixer.set_num_channels(1)
 	musicwant = random.randint(0,len(musicdir)-1)
-	mixer.music.load(musicdir[musicwant])
 	print(f'Playing {musicdir[musicwant].split(".")[0]}')
 	speakmay(f'Playing {musicdir[musicwant].split(".")[0]}')
+	mixer.music.load(musicdir[musicwant])
 	# mixer.music.fadeout(10000)
 	mixer.music.play(loop)
 
@@ -120,10 +154,16 @@ if __name__ == '__main__':
 
 
 	while True:
+		breakingnum = 0
 		while True:
 			say = listen()
-			if say is not None:
+			if say is not None and breakingnum < 5:
+				breakingnum = 0
 				break
+			elif breakingnum >= 5  and mixer.music.get_busy()==0 :
+				exit()
+			else:
+				breakingnum += 1
 		# wants = input("\n\nEnter: ").lower()
 		wants = say.lower()
 		# listen()
@@ -131,11 +171,18 @@ if __name__ == '__main__':
 		if wants == "exit" or 'bye' in wants:
 			break
 
+		elif "sleep" in wants:
+			if "min" in wants:
+				time.sleep(int(re.findall(r'\d+',wants)[0])*60)
+			else:
+				time.sleep(int(re.findall(r'\d+',wants)[0]))
+
+
 		elif "wikipedia" in wants:
 			try:
 				speakmay("Found this from Wikipedia.")
 				# find = wants.split(" ")[0]
-				results = wikipedia.summary(wants,sentences=2)
+				results = wikipedia.summary(wants.replace("wikipedia",""),sentences=2)
 				print(results)
 				speakmay(results)
 
@@ -185,73 +232,16 @@ if __name__ == '__main__':
 			elif "resume" in wants:
 				mixer.music.unpause()
 
-
-		# Using os module and playing in Windows Media Player --------
-		# elif "play" in wants and  "music" in wants:
-			# musicdir = [ item for item in os.listdir(r'F:\Anik\My Musics\English musics') if os.path.splitext(item)[1] == ".mp3"]
-			# for index,item in enumerate(musicdir,1):
-			# 	print(f'{index}. {item}')
-			# musicwant = int(input("\nWhich song you want to play!?\n"))
-			# os.startfile(os.path.join(r'F:\Anik\My Musics\English musics',musicdir[musicwant-1]))
-
 		elif "time" in wants:
 			timenow = f'Time is {time.strftime("%I:%M%p",time.localtime())}.'
 			print(timenow)
 			speakmay(timenow)
-
-		elif "date" in wants or "today" in wants:
-			todayday = time.strftime("%A",time.localtime())
-			todaydate = time.strftime("%d %B",time.localtime())
-			daydatewish = f'Today is {todayday} and Date is {todaydate}.'
-			print(daydatewish)
-			speakmay(daydatewish)
 
 		elif "weather" in wants:
 			try:
 				speakmay(myweathercalls.OpenweatherAPIcalltext())
 			except Exception as e:
 				print("Couldn't fetch weather. Try again.")
-
-		elif "open" in wants: # and "sublime" in wants:
-			# findd = re.compile(r'')
-			# openwhat = re.findall(r'open \S+ \S',str2)[0].split(" ")
-
-			if "google" in wants:
-				webbrowser.open("www.google.com")
-
-			elif "youtube" in wants:
-				webbrowser.open("www.youtube.com")
-
-			elif "whatsapp" in wants:
-				webbrowser.open("https://web.whatsapp.com")
-
-			elif "chrome" in wants:
-				os.startfile("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe")
-
-		elif "search google" in wants:
-			url = f'https://www.google.com/search?q={wants.replace("search ","").replace("google ","").replace(" ","+")}'
-			webbrowser.open_new_tab(url)
-
-		elif "search youtube" in wants:
-			url = f'https://www.youtube.com/results?search_query={wants.replace("search ","").replace("youtube ","").replace(" ","+")}'
-			webbrowser.open_new_tab(url)
-
-		elif "joke" in wants:
-			try:
-				url = "https://api.yomomma.info/"
-				joketext = requests.get(url)
-				jokejson = json.loads(joketext.text)
-				speakmay(jokejson['joke'])
-			except Exception as e:
-				print("Couldn't get joke. Cause your life's a joke.")
-
-		elif "insult me" in wants:
-			try:
-				url = "https://evilinsult.com/generate_insult.php"
-				insulttext = requests.get(url).text
-				speakmay(insulttext)
-			except Exception as e:
-				print("Couldn't get Insult. Cause your life's a joke.")
 
 		elif "news" in wants:
 			try:
@@ -284,6 +274,75 @@ if __name__ == '__main__':
 			except Exception as e:
 				print("Couldn't get news. Try again.")
 			
+		elif "date" in wants or "today" in wants:
+			todayday = time.strftime("%A",time.localtime())
+			todaydate = time.strftime("%d %B",time.localtime())
+			daydatewish = f'Today is {todayday} and Date is {todaydate}.'
+			print(daydatewish)
+			speakmay(daydatewish)
+
+		elif "open" in wants: # and "sublime" in wants:
+			# findd = re.compile(r'')
+			# openwhat = re.findall(r'open \S+ \S',str2)[0].split(" ")
+
+			if "google" in wants:
+				webbrowser.open("www.google.com")
+
+			elif "youtube" in wants:
+				webbrowser.open("www.youtube.com")
+
+			elif "whatsapp" in wants:
+				webbrowser.open("https://web.whatsapp.com")
+
+			elif "chrome" in wants:
+				os.startfile("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe")
+
+			elif "sublime" in wants:
+				os.startfile(r'C:\Program Files\Sublime Text 3\sublime_text.exe')
+
+
+		elif "play" in wants and "youtube" in wants:
+			url = ytfirsturlreturn(wants.replace("play","").replace("youtube","").replace("on",""))
+			speakmay(f"Playing {pafy.new(url).title} on Youtube")
+			webbrowser.open_new_tab(url)
+
+		elif "search" in wants and "google" in wants:
+			url = f'https://www.google.com/search?q={wants.replace("search ","").replace("google ","").replace(" ","+")}'
+			webbrowser.open_new_tab(url)
+
+		elif "search" in wants and "youtube" in wants:
+			url = f'https://www.youtube.com/results?search_query={wants.replace("search ","").replace("youtube ","").replace(" ","+")}'
+			webbrowser.open_new_tab(url)
+
+		elif "search" in wants:
+			try:
+				results = wikipedia.summary(wants.replace("search",""),sentences=2)
+				if "==" in results:
+					print(wikipedia.summary(wants.replace("search","")))
+					speakmay(wikipedia.summary(wants.replace("search","")))
+				else:
+					print(results)
+					speakmay(results)
+			except Exception as e:
+				print(e)
+
+		elif "joke" in wants:
+			try:
+				url = "https://api.yomomma.info/"
+				joketext = requests.get(url)
+				jokejson = json.loads(joketext.text)
+				speakmay(jokejson['joke'])
+			except Exception as e:
+				print("Couldn't get joke. Cause your life's a joke.")
+
+		elif "insult me" in wants:
+			try:
+				url = "https://evilinsult.com/generate_insult.php"
+				insulttext = requests.get(url).text
+				speakmay(insulttext)
+			except Exception as e:
+				print("Couldn't get Insult. Cause your life's a joke.")
+		
 		elif "quote" in wants:
 			try:
 				quote = quotereturn()[0]
@@ -304,6 +363,9 @@ if __name__ == '__main__':
 				alarmthread.start()
 			except Exception as e:
 				print("Couldn't set alarm/reminder.")
+
+		elif "say" in wants[0:4] or "speak" in wants[0:6]:
+			speakmay(wants.replace("say","").replace("speak",""))
 
 		else:
 			speakmay("Sorry, I can't help with this.")
